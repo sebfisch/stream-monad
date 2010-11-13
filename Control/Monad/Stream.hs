578347661dbab32bbe2f3c3d515c1ac14d08eb5f
Warren Harris <warrensomebody@gmail.com>
2010-11-14 04:18:47 +0800

    Added runL, ifte, once, and cut.

--- Control/Monad/Stream.hs
+++ Control/Monad/Stream.hs
@@ -24,7 +24,8 @@
 -- are interpreted as a multiset, i.e., a valid transformation
 -- according to the monad laws may change the order of the results.
 -- 
-module Control.Monad.Stream ( Stream, suspended, runStream ) where
+module Control.Monad.Stream ( Stream, suspended, runStream, 
+                              runL, ifte, once, cut ) where
 
 import Control.Monad
 import Control.Applicative
@@ -57,6 +58,56 @@ runStream Nil         = []
 runStream (Single x)  = [x]
 runStream (Cons x xs) = x : runStream xs
 runStream (Susp xs)   = runStream xs
+
+msplit :: Stream a -> Maybe (a, Stream a)
+msplit Nil = Nothing
+msplit (Single a) = Just (a, Nil)
+msplit (Cons a r) = Just (a, r)
+msplit (Susp i) = msplit i
+
+-- |
+-- The function @runL@ enumerates the results of a
+-- non-deterministic computation, limited by an optional count.
+-- 
+runL :: Maybe Int -> Stream a -> [a]
+runL Nothing m = case msplit m of
+  Nothing -> []
+  Just (a, r) -> a:runL Nothing r
+runL (Just i) _ | i == 0 = []
+runL (Just i) m = case msplit m of
+  Nothing -> []
+  Just (a, r) -> a:runL (Just (i-1)) r
+
+-- | 
+-- The function @ifte t th el@ is the logical conditional operator,
+-- equivalent to Prolog's "soft-cut". First the computation @t@ is
+-- executed. If it succeeds with at least one result, the entire
+-- @ifte@ computation is equivalent to @t >>= th@. Otherwise, the
+-- entire computation becomes equivalent to @el@.
+--
+ifte :: Stream a -> (a -> Stream b) -> Stream b -> Stream b
+ifte t th el = case msplit t of 
+  Nothing -> el
+  Just (a, r) -> th a `mplus` (r >>= th)
+
+-- | 
+-- The function @once@ selects one solution out of possibly
+-- many. It greatly improves efficiency as it can be used to avoid
+-- useless backtracking and therefore to dispose of data structures
+-- that hold information needed for backtracking (e.g., choice
+-- points).
+--
+once :: Stream a -> Stream a
+once m = case msplit m of
+  Nothing -> mzero
+  Just (a, _) -> return a
+
+-- |
+-- The function @cut@ implements “negation as failure” and is
+-- equivalent to "cut" in Prolog.
+--
+cut :: Stream a -> Stream ()
+cut m = ifte (once m) (const mzero) (return ())
 
 instance Monad Stream
  where
